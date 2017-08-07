391994666da197553d0ab4960a861d1f6e23c017
leon-good-life <leon.good.life@gmail.com>
2017-08-07 12:26:03 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # swipe-react
 Easy integration!!! Easily add touch devices support (smartphones, tablets) to your react components, with swipe direction detection.
+
+# Usage
+1. Install the npm package:
+```bash
+    npm install --save swipe-react
+```
+2. Import it:
+```javascript
+    import SwipeReact from 'swipe-react';
+```
+3. Config swipe events ('left', 'right', 'up', 'down'), at least one of them, in your component constructor, or in render function:
+```javascript
+    SwipeReact.config({
+      left: () => {
+        console.log('Swipe left detected.');
+      },
+      right: () => {
+        console.log('Swipe right detected.');
+      },
+      up: () => {
+        console.log('Swipe up detected.');
+      },
+      down: () => {
+        console.log('Swipe down detected.');
+      }
+    });
+```
+
+4. Integrate with your React component:
+```javascript
+  <YourComponent {...SwipeReact.events} />
+```
---
+++ SwipeReact.js
@@ -0,0 +1,60 @@
+let SwipeReact = {};
+
+SwipeReact.xDown = null;
+SwipeReact.yDown = null;
+
+SwipeReact.configuration = {};
+
+SwipeReact.config = (config)=>{
+  const options = ['right', 'left', 'up', 'down'];
+  for (let option of options) {
+    if (config.hasOwnProperty(option)) {
+      SwipeReact.configuration[option] = config[option];
+    }
+  }
+};
+
+SwipeReact.events = {
+  onTouchStart: (e) => {
+    SwipeReact.xDown = e.touches[0].clientX;
+    SwipeReact.yDown = e.touches[0].clientY;
+  },
+  onTouchMove: (e) => {
+
+    if (SwipeReact.xDown === null || SwipeReact.yDown === null) {
+        return;
+    }
+
+    let xUp = e.touches[0].clientX;
+    let yUp = e.touches[0].clientY;
+
+    let xDiff = SwipeReact.xDown - xUp;
+    let yDiff = SwipeReact.yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0){
+          if(SwipeReact.configuration.hasOwnProperty('right')){
+            SwipeReact.configuration.right();
+          }
+        } else{
+          if(SwipeReact.configuration.hasOwnProperty('left')){
+            SwipeReact.configuration.left();
+          }
+        }
+    } else {
+        if (yDiff > 0) {
+          if(SwipeReact.configuration.hasOwnProperty('up')){
+            SwipeReact.configuration.up();
+          }
+        } else {
+          if(SwipeReact.configuration.hasOwnProperty('down')){
+            SwipeReact.configuration.down();
+          }
+        }
+    }
+    SwipeReact.xDown = null;
+    SwipeReact.yDown = null;
+  }
+};
+
+export default SwipeReact;
